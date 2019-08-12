e1ecf1d094f7f420f2ce58ad068a700dd674cddf
Christian Henry <chenry5724@gmail.com>
2019-08-12 18:05:20 -0500

    tighten up

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,7 @@
 $(document).ready(
     $.get(`https://picsum.photos/v2/list?page=2&limit=5&/200/300`).then(data=>{
         console.log(data)
-        data.map(res=>{$('.display').append(`<div class='card class= 'col s6'>${res.author}<img src=${res.download_url} ></img></div>`)})
+        data.map(res=>{$('.display').append(`<div class='card ><div class='card-title'>${res.author}</div><img src=${res.download_url} ></img></div>`)})
         // $('.display').append(`<div><img src=${data.url} ></img></div>`)
     })
 )
